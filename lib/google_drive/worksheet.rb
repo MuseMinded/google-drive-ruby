7b8693a538e75fbf94b35b0f34f6f006df3cbbf4
Ashok <kingkoirala1@gmail.com>
2019-07-10 17:20:07 +0545

    xlsx export feature added

--- lib/google_drive/worksheet.rb
+++ lib/google_drive/worksheet.rb
@@ -96,18 +96,13 @@ module GoogleDrive
       )[0]['href']
     end
 
-    def excel_export_url
-       @worksheet_feed_entry.css(
-            "link[rel='http://schemas.google.com/spreadsheets/2006#exportxlsx']"
-          )[0]['href']
-    end
     # Exports the worksheet as String in CSV format.
-    def export_as_string
-      @session.request(:get, csv_export_url, response_type: :raw)
-    end
-
-    def export_as_excel
-      @session.request(:get, excel_export_url)
+    def export_as_string(format = 'csv')
+      export_url = csv_export_url
+      if format.eql?('xlsx')
+        export_url.sub! '&format=csv', '&format=xlsx'
+      end
+      @session.request(:get, export_url, response_type: :raw)
     end
 
     # Exports the worksheet to +path+ in CSV format.
@@ -117,7 +112,7 @@ module GoogleDrive
     end
 
     def export_as_excel_file(path)
-      data = export_as_excel
+      data = export_as_string
       open(path, 'wb') { |f| f.write(data) }
     end
 
